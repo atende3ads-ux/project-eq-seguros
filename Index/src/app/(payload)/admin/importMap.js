877f2c4faa8b7b6c2bedb24de11a3eb5f03f2799
3ADS <atende3ads@gmail.com>
2026-09-17 15:03:58 -0300

    feat: adiciona Index/src/app/(payload)/admin

---
+++ Index/src/app/(payload)/admin/importMap.js
@@ -0,0 +1,8 @@
+import { default as default_cb7984f573641b76de8e002f97c0e2ef } from '../../../components/admin/PasswordVisibility'
+import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
+
+/** @type import('payload').ImportMap */
+export const importMap = {
+  "/components/admin/PasswordVisibility#default": default_cb7984f573641b76de8e002f97c0e2ef,
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
+}
